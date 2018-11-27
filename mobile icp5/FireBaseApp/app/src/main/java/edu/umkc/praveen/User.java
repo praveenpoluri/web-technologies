72fd018c04a4302db0904246f731d0bd9f1567fb
praveenpoluri <42703022+praveenpoluri@users.noreply.github.com>
2018-11-26 22:51:05 -0600

    mobileicp5

---
+++ mobile icp5/FireBaseApp/app/src/main/java/edu/umkc/praveen/User.java
@@ -0,0 +1,22 @@
+package edu.umkc.praveen;
+
+/**
+ * Created by Rashmi on 4/16/2018.
+ */
+
+public class User {
+
+        public String name;
+        public String phone;
+
+        // Default constructor required for calls to
+        // DataSnapshot.getValue(User.class)
+        public User() {
+        }
+
+        public User(String name, String phone) {
+            this.name = name;
+            this.phone = phone;
+        }
+}
+
